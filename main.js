6ba80b8c65fd8649a723566024b98a70e886404b
Niraj Aryal <niroz.aryal11@gmail.com>
2019-05-16 21:02:32 -0600

    Appends card on click

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ var bodyInput = document.querySelector('.body-input');
 var cardSection = document.querySelector('.card-section')
 
 
-button.addEventListener('click', function(){
-	alert("heyyy")
+button.addEventListener('click', () => {
+	createCard(event)
 })
+
+createCard = (event) => {
+	event.preventDefault()
+	var card =
+    `<div class="card-display">
+      <h2>title</h2>
+      <p>this is a card body</p>
+      <div class="card-button">
+        <button class="up-vote btn">up</button>
+        <button class="down-vote btn">Down</button>
+        <p class="card-text"></p>
+        <button class="delete-button" > X </button>
+      </div>
+     </div>`
+	cardSection.innerHTML += card;
+}
+
