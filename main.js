b4897bf2ee8965add82414cd2988fd9483f9139c
Niraj Aryal <niroz.aryal11@gmail.com>
2019-05-16 20:54:36 -0600

    Gets elements from input-form

---
+++ main.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('.submit-btn');
+var titleInput = document.querySelector('.title-input');
+var bodyInput = document.querySelector('.body-input');
+var cardSection = document.querySelector('.card-section')
+
+
+button.addEventListener('click', function(){
+	alert("heyyy")
+})
